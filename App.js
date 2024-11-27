9a4d079a56e090a696a4960daf2bc530c652dacc
benyyyy <106058071+benyyyy@users.noreply.github.com>
2024-11-27 12:53:20 +0530

    this is second time git hub update

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-   
-   const parent = React.createElement("div",{id:"parent"},React.createElement
-    ("div",{id:"child"},[React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")]))
-   
-   // Create a React element
-   const heading = React.createElement("h1", {
-    id:"heading",xyz: "abc"
-   }, "Hello World");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-   // Create the root and render the element
-   const root = ReactDOM.createRoot
-   (document.getElementById("first"));
-   root.render(headings);
-  
+// Title Component
+const Title = () => (
+  <h1>This is another component</h1>
+);
 
+// JSX Element
+const jsxHeading = <h1 id="heading">Hello, how are you?</h1>;
+console.log(jsxHeading);
 
-  
+// Functional Component
+const FunctionalComponent = () => {
+  // Ensure you use `return` to provide the JSX
+ return  (
+    <div>
+      <h1>I am a functional component of React</h1>
+      <Title /> {/* Use the Title component */}
+      //we can also call like this 
+      {Title()} 
+    </div>
+  );
+};
+
+// Create root
+const root = ReactDOM.createRoot(document.getElementById("first"));
+root.render(<FunctionalComponent />);
